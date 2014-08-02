3133be8b4abdbc6c20deff70f0224901663cf686
Bryan White <bryanchriswhite@gmail.com>
2014-08-02 18:40:12 +0200

    `yo angular TruecoinDemo`

---
+++ demo-frontend/app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name truecoinDemoApp
+ * @description
+ * # truecoinDemoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('truecoinDemoApp', [
+    'ngCookies',
+    'ngSanitize'
+  ]);
---
+++ demo-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name truecoinDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the truecoinDemoApp
+ */
+angular.module('truecoinDemoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
