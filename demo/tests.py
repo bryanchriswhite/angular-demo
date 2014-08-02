983e601d44bcf5333499178abae9ba35d7733eb4
Bryan White <bryanchriswhite@gmail.com>
2014-08-02 17:26:51 +0200

    sanity check on `unittest`

--- demo/tests.py
+++ demo/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from demo.models import Product
 
-# Create your tests here.
+
+class SanityCheckTestCase(TestCase):
+    def setUp(self):
+        Product.objects.create(
+            name='First Product',
+            description='This is the description for the first product.',
+            inventory_count=15
+        )
+
+    def test_djangoWorks(self):
+        product = Product.objects.all()[0]
+        self.assertEqual(product.name, 'First Product')
+
