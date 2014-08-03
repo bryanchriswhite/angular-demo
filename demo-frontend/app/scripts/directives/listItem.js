0c99c1862c31f8d1c5a3e719fd275fe8e6105751
Bryan White <bryanchriswhite@gmail.com>
2014-08-03 03:55:20 +0200

    * switch to inherited scope in tc-list-item * some styling * copy product data to prevent mutation of original resoruce

--- demo-frontend/app/scripts/directives/listItem.js
+++ demo-frontend/app/scripts/directives/listItem.js
@@ -1,13 +1,15 @@
 angular.module('TruecoinDemoApp.directives')
   .directive('tcListItem', function($compile) {
     return {
-      scope: {
-        product: '=',
-      },
+      scope: true,
       link : function(scope, element, attrs) {
-        var actions = angular.element('<div class="fade out input-group">\
-        <span class="input-group-addon "><span class="glyphicon glyphicon-ok"></span></span>\
-        <input class="form-control" type="text"/>\
+        var actions = angular.element('<div class="list-item-form fade out"><div class="input-group">\
+        <span class="input-group-addon" ng-click="closeForm()"><span class="glyphicon glyphicon-remove"></span></span>\
+        <input class="form-control" type="text" ng-model="productCopy.name"/>\
+        <input class="form-control" type="text" ng-model="productCopy.description"/>\
+        <input class="form-control" type="text" ng-model="productCopy.inventory_count" pattern="\\d+"/>\
+        <span class="input-group-addon" ng-click="submit()"><span class="glyphicon glyphicon-ok"></span></span>\
+        </div>\
         </div>\
         <div class="list-item-buttons fade out btn-group">\
           <button ng-click="openForm()" class="btn btn-warning"><span class="glyphicon glyphicon-pencil"></span></button>\
@@ -18,7 +20,7 @@ angular.module('TruecoinDemoApp.directives')
         $compile(actions)(scope);
 
         var buttons = element.find('.list-item-buttons')
-          , form = element.find('.input-group')
+          , form = element.find('.list-item-form')
           , hide = function(_element) {
             _element.toggleClass('out', true);
             _element.toggleClass('in', false);
@@ -30,12 +32,19 @@ angular.module('TruecoinDemoApp.directives')
           , setStyle = function(_element, property, value) {
             _element[0].style[property] = value;
           }
+          , copyProductData = function() {
+            scope.productUpdate = {
+              name           : scope.product.name,
+              description    : scope.product.description,
+              inventory_count: scope.product.inventory_count
+            }
+          }
           ;
 
         element.on('mouseenter', function() {
           //-- don't show the buttons if a form is open
           if (scope.formOpen) return;
-          
+
           show(buttons);
           var top = element.position().top + (element.height() / 2) + 'px'; //event.clientY + 'px';
           var left = event.clientX + 10 + 'px';
@@ -55,7 +64,19 @@ angular.module('TruecoinDemoApp.directives')
           scope.formOpen = true;
           hide(buttons);
           show(form);
-        }
+        };
+
+        scope.closeForm = function() {
+          scope.formOpen = false;
+          show(buttons);
+          hide(form);
+        };
+        
+        scope.submit = function() {
+          scope.product.save()
+        };
+
+        copyProductData();
       }
     }
   });
