57c5c175f71d3f2d249d3d7d89aafef5d5f33db7
Bryan White <bryanchriswhite@gmail.com>
2014-08-04 02:37:47 +0200

    UI improvements

--- demo-frontend/app/scripts/directives/productForm.js
+++ demo-frontend/app/scripts/directives/productForm.js
@@ -1,39 +1,43 @@
 angular.module('TruecoinDemoApp.directives')
-  .directive('tcProductForm', function($compile, $templateCache) {
+  .directive('tcProductForm', function($compile, $templateCache, $timeout) {
     return {
       restrict: 'A',
-      scope: {
+      scope   : {
         formOpen: '=',
-        model: '=formModel',
+        model   : '=formModel',
         position: '=formPosition'
       },
-      link : function(scope, element, attrs) {
+      link    : function(scope, element, attrs) {
         var template = angular.element($templateCache.get('product-form'));
 
         element.after(template);
         $compile(template)(scope);
-        
+
         scope.$watch('formOpen', function(newVal, oldVal) {
           if (newVal === true) {
             template[0].style.display = 'block';
-            template.toggleClass('in', true);
-            template.toggleClass('out', false);
+            $timeout(function() {
+              template.toggleClass('in', true);
+              template.toggleClass('out', false);
+            }, 100);
           } else {
             template.toggleClass('in', false);
             template.toggleClass('out', true);
-            template[0].style.display = 'none';
+            $timeout(function() {
+              template[0].style.display = 'none';
+            }, 500);
           }
         });
-        
+
         scope.$watch('position', function(newVal) {
           template[0].style.top = newVal.top + 'px';
           template[0].style.left = newVal.left + 'px';
         });
-        
+
         scope.closeForm = function() {
           scope.formOpen = false;
         };
-        
+
         scope.submit = function() {
           console.log('submitted');
         };
